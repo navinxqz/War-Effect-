bbbb3d11c44e4dfaa50d832026b66e83e0219faa
Navin <navinmdnawshin@gmail.com>
2025-06-24 23:20:02 +0600

    sound

--- WarEffect/Source.cpp
+++ WarEffect/Source.cpp
@@ -5,17 +5,22 @@
 #include <math.h>
 #include <Windows.h>
 #include <mmsystem.h>
+
 #pragma comment(lib,"Winmm.lib")
 
-GLuint sceneTexture, tankTexture, missileTexture, copperTexture, fireTexture, artilleryTexture;
+GLuint sceneTexture, scene2Texture, tankTexture, newTankTexture, missileTexture, copperTexture, fireTexture, artilleryTexture;
 int tankImgWidth, tankImgHeight;
+int newTankImgWidth, newTankImgHeight;
 int missileImgWidth, missileImgHeight;
 int copperImgWidth, copperImgHeight;
 int fireImgWidth, fireImgHeight;
 int artilleryImgWidth, artilleryImgHeight;
 
+int current = 1;
+
 // Tank variables
 float tankX = 1.2f, tankY = 0.18f;
+float newTankX = 1.5f, newTankY = 0.18f;
 float tankSpeedX = -0.002f, tankSpeedY = -0.00105f;
 bool stop = false;
 
@@ -39,6 +44,12 @@ float artilleryX = -0.0f, artilleryY = -0.8f;
 bool artilleryFireVisible = false;
 float artilleryFireOffsetX = -0.15f, artilleryFireOffsetY = 0.15f;
 
+// Camera shake variables
+float cameraShakeOffsetX = 0.0f;
+float cameraShakeOffsetY = 0.0f;
+int cameraShakeFramesLeft = 0;
+const int maxShakeFrames = 6;
+
 
 // Flame variables
 float flameTime = 0.0f;
@@ -53,6 +64,13 @@ FlamePos flames[] = {
     { 0.3f, -0.05f}
 };
 int flameCount = sizeof(flames) / sizeof(flames[0]);
+
+const int NUM_RAINDROPS = 200;
+struct Raindrop {
+    float x, y;
+    float speedY;
+};
+Raindrop raindrops[NUM_RAINDROPS];
 
 // Load texture from file
 GLuint loadTexture(const char* filename, int* outWidth = nullptr, int* outHeight = nullptr) {
@@ -70,7 +88,10 @@ GLuint loadTexture(const char* filename, int* outWidth = nullptr, int* outHeight
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
     GLenum format = (channels == 4) ? GL_RGBA : GL_RGB;
-    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, image);
+    //glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, image);
+    GLint internalFormat = (channels == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, image);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     stbi_image_free(image);
@@ -81,19 +102,68 @@ GLuint loadTexture(const char* filename, int* outWidth = nullptr, int* outHeight
 void changeFireTexture(int value);
 void hideFireTexture(int value);
 
+void initRain() {
+    for (int i = 0; i < NUM_RAINDROPS; i++) {
+        raindrops[i].x = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;    // X in [-1, 1]
+        raindrops[i].y = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;    // Y in [-1, 1]
+        raindrops[i].speedY = 0.01f + ((float)rand() / RAND_MAX) * 0.02f;
+    }
+}
+void drawRain() {
+    glDisable(GL_TEXTURE_2D);
+    glColor4f(0.6f, 0.6f, 1.0f, 0.5f);
+    glLineWidth(1.5f);
+    glBegin(GL_LINES);
+    for (int i = 0; i < NUM_RAINDROPS; i++) {
+        float x = raindrops[i].x;
+        float y = raindrops[i].y;
+        glVertex2f(x, y);
+        glVertex2f(x, y - 0.05f);    // Raindrop length
+    }
+    glEnd();
+    glEnable(GL_TEXTURE_2D);
+}
+void updateRain() {
+    for (int i = 0; i < NUM_RAINDROPS; i++) {
+        raindrops[i].y -= raindrops[i].speedY;
+        if (raindrops[i].y < -1.0f) {
+            raindrops[i].y = 1.0f;   // reset to top
+            raindrops[i].x = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;  // random x again
+        }
+    }
+}
+void triggerCameraShake(int numFrames) {
+    cameraShakeOffsetX = ((float)rand() / RAND_MAX - 0.5f) * 0.04f; // Random between -0.04 and 0.04
+    cameraShakeOffsetY = ((float)rand() / RAND_MAX - 0.5f) * 0.02f;
+    cameraShakeFramesLeft = numFrames;
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     glEnable(GL_TEXTURE_2D);
 
+    if (current == 1) {
+        glBindTexture(GL_TEXTURE_2D, sceneTexture);
+    }
+    else if (current == 2) {
+        glBindTexture(GL_TEXTURE_2D, scene2Texture);
+    }glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+    glPushMatrix();
+    glTranslatef(cameraShakeOffsetX, cameraShakeOffsetY, 0.0f);
+
     // Draw background
-    glBindTexture(GL_TEXTURE_2D, sceneTexture);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0f, -1.0f);
     glTexCoord2f(1.0f, 0.0f); glVertex2f(1.0f, -1.0f);
     glTexCoord2f(1.0f, 1.0f); glVertex2f(1.0f, 1.0f);
     glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0f, 1.0f);
     glEnd();
+
+    if (current == 2) {
+        drawRain();
+    }
 
     // Check fire visible
     if (fireVisible) {
@@ -127,6 +197,22 @@ void display() {
     glTexCoord2f(1.0f, 0.0f); glVertex2f(displayWidth / 2, -displayHeight / 2);
     glTexCoord2f(1.0f, 1.0f); glVertex2f(displayWidth / 2, displayHeight / 2);
     glTexCoord2f(0.0f, 1.0f); glVertex2f(-displayWidth / 2, displayHeight / 2);
+    glEnd();
+    glPopMatrix();
+
+    // Draw new tank
+    glPushMatrix();
+    glTranslatef(newTankX, newTankY, 0.0f);
+    glBindTexture(GL_TEXTURE_2D, newTankTexture);
+    float newTankAspect = (float)newTankImgWidth / (float)newTankImgHeight;
+    float newTankDisplayWidth = 0.2f;
+    float newTankDisplayHeight = newTankDisplayWidth / newTankAspect;
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f); glVertex2f(-newTankDisplayWidth / 2, -newTankDisplayHeight / 2);
+    glTexCoord2f(1.0f, 0.0f); glVertex2f(newTankDisplayWidth / 2, -newTankDisplayHeight / 2);
+    glTexCoord2f(1.0f, 1.0f); glVertex2f(newTankDisplayWidth / 2, newTankDisplayHeight / 2);
+    glTexCoord2f(0.0f, 1.0f); glVertex2f(-newTankDisplayWidth / 2, newTankDisplayHeight / 2);
     glEnd();
     glPopMatrix();
 
@@ -220,6 +306,7 @@ void display() {
         //drawFlame(flames[i].x, flames[i].y);
     }
     glutSwapBuffers();
+    glPopMatrix();
 }
 void changeFireTexture(int value);
 void hideFireTexture(int value);
@@ -227,13 +314,34 @@ void hideFireTexture(int value);
 //bool fireVisible = false;
 //float flameTime = 0.0f;
 bool fireSequenceActive = false;
+bool copperSoundPlaying = false;
+
+//sound part
+void playTankFireSound() {
+    PlaySound(TEXT("./assets/tankfire7.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+void playArtilleryFireSound() {
+    PlaySound(TEXT("./assets/tankfire1.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+void copperSound() {
+    PlaySound(TEXT("./assets/copper.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+void playRainSound() {
+    PlaySound(TEXT("./assets/rain.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+}
 
 void update(int value) {
+    if (current == 2) {
+        updateRain();
+    }
     flameTime += 0.1f;
+    newTankX += tankSpeedX;
+    newTankY += tankSpeedY;
 
     if (!stop) {
         tankX += tankSpeedX;
         tankY += tankSpeedY;
+
         if (tankX <= 0.2f) {
             stop = true;
 
@@ -256,12 +364,32 @@ void update(int value) {
     copperX += copperSpeedX;
     copperY += copperSpeedY;
 
+    if (copperX > -1.0f && copperX < 0.45f){
+        if (!copperSoundPlaying) {
+            copperSound();
+            copperSoundPlaying = true;
+        }
+    }else {
+        if (copperSoundPlaying) {
+            PlaySound(NULL, NULL, 0);
+            copperSoundPlaying = false;
+        }
+    }
+
     if (copperX > 1.0f) {
         copperX = -1.0f;
         copperY = -0.4f;
     }
     rotorAngle += 10.0f;
     if (rotorAngle > 360.0f) rotorAngle -= 360.0f;
+
+    if (cameraShakeFramesLeft > 0) {
+        cameraShakeFramesLeft--;
+        if (cameraShakeFramesLeft == 0) {
+            cameraShakeOffsetX = 0.0f;
+            cameraShakeOffsetY = 0.0f;
+        }
+    }
 
     glutPostRedisplay();
     glutTimerFunc(16, update, 0);
@@ -278,47 +406,54 @@ void hideFireTexture(int value) {
     fireVisible = false;
     glutPostRedisplay();
 }
-//sound part
-void playTankFireSound() {
-    PlaySound(TEXT("./assets/tankfire7.wav"), NULL, SND_FILENAME | SND_ASYNC);
-}
-void playArtilleryFireSound() {
-    PlaySound(TEXT("./assets/tankfire1.wav"), NULL, SND_FILENAME | SND_ASYNC);
-}
 //keyboard part
 void keyboard(unsigned char key, int x, int y) {
-    if (key == 'F' || key == 'f'){
-
-        fireVisible = true;
-        if (fireTexture != 0){ glDeleteTextures(1, &fireTexture); }
-        fireTexture = loadTexture("./assets/TankFire1.png", &fireImgWidth, &fireImgHeight);
-        playTankFireSound();
-
-        glutTimerFunc(1000, changeFireTexture, 0);
-        glutTimerFunc(2000, hideFireTexture, 0);
-        glutPostRedisplay();
+    if (key == '1') {
+        current = 1;
+        PlaySound(NULL, NULL, 0);
     }
-    else if(key == 'G' || key == 'g') {
-        artilleryFireVisible = true;
-        fireTexture = loadTexture("./assets/TankFire1.png", &fireImgWidth, &fireImgHeight);
-        playArtilleryFireSound();
+    else if (key == '2') {
+        current = 2;
+        initRain();
+        playRainSound();
+    }
+    if (current == 1) {
+        if (key == 'F' || key == 'f') {
+            fireVisible = true;
+            if (fireTexture != 0) { glDeleteTextures(1, &fireTexture); }
+            fireTexture = loadTexture("./assets/TankFire1.png", &fireImgWidth, &fireImgHeight);
+            playTankFireSound();
+            triggerCameraShake(maxShakeFrames); 
 
-        glutTimerFunc(1000, [](int) { artilleryFireVisible = false; glutPostRedisplay(); }, 0);
-        glutPostRedisplay();
+            glutTimerFunc(1000, changeFireTexture, 0);
+            glutTimerFunc(2000, hideFireTexture, 0);
+            glutPostRedisplay();
+        }
+        else if (key == 'G' || key == 'g') {
+            artilleryFireVisible = true;
+            fireTexture = loadTexture("./assets/TankFire1.png", &fireImgWidth, &fireImgHeight);
+            playArtilleryFireSound();
+            triggerCameraShake(maxShakeFrames);
+
+            glutTimerFunc(1000, [](int) { artilleryFireVisible = false; glutPostRedisplay(); }, 0);
+            glutPostRedisplay();
+        }
     }
 }
 void init() {
-    glClearColor(0,0,0,1);
+    glClearColor(1.0f,1.0f,1.0f,1.0f);
     stbi_set_flip_vertically_on_load(true);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     sceneTexture = loadTexture("./assets/scene3.png");
+    scene2Texture = loadTexture("./assets/rainy.png");
     tankTexture = loadTexture("./assets/tankangle.png", &tankImgWidth, &tankImgHeight);
     missileTexture = loadTexture("./assets/Missile.png", &missileImgWidth, &missileImgHeight);
     copperTexture = loadTexture("./assets/copperback.png", &copperImgWidth, &copperImgHeight);
     fireTexture = loadTexture("./assets/TankFire1.png", &fireImgWidth, &fireImgHeight);
     artilleryTexture = loadTexture("./assets/artillery-gun.png", &artilleryImgWidth, &artilleryImgHeight);
+    newTankTexture = loadTexture("./assets/tank.png", &newTankImgWidth, &newTankImgHeight); // Load new tank texture
 
     glutKeyboardFunc(keyboard);
 }
